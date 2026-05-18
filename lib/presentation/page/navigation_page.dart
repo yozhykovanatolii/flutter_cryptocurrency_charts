e67d7f64397df873e034183fbd8135c0fce1dae8
Yozhykov Anatolii <yozhykovanatolii@gmail.com>
2026-05-18 00:18:06 -0700

    Refactor deprecated methods and classes to the existing one

--- lib/presentation/page/navigation_page.dart
+++ lib/presentation/page/navigation_page.dart
@@ -33,10 +33,7 @@ class _NavigationPageState extends State<NavigationPage> {
 
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () async {
-        return false;
-      },
+    return PopScope(
       child: Builder(builder: (BuildContext context) {
         return BlocBuilder<InitialDataBloc, InitialDataState>(
             bloc: initialDataBloc,
